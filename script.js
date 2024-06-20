d18d38fc6653b3d5f50053e480e8b8edcc7d92a8
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-06-21 03:18:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// function showAlert() {
+//     var form = document.forms[0];
+//     var firstName = form.elements["First Name"].value;
+//     var lastName = form.elements["Last Name"].value;
+//     var phoneNumber = form.elements["Phone Number"].value;
+//     var emailId = form.elements["Email ID"].value;
+    
+//     alert("First Name: " + firstName + "\n" +
+//           "Last Name: " + lastName + "\n" +
+//           "Phone Number: " + phoneNumber + "\n" +
+//           "Email ID: " + emailId);
+// }
